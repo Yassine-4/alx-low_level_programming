79526a650dbc702337853c97e1a697c5c0c73387
Yassine-4 <Yassine-4@gmail.com>
2023-10-05 19:55:52 -0700

    i

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char n[] = "Programming is like building a multilingual puzzle\n" ;
+	puts (n);
+	return (0);
+}
