9436aa0d8e5f7a38adce3cf0ef98072790938860
Yassine-4 <Yassine-4@gmail.com>
2023-10-09 16:53:42 -0700

    fix

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	printf("%c ", c);
+	printf("\n");
+	return (0);
+}
