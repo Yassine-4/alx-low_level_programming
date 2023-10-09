633f09600920dbe65095eec9a19022512d1306bd
Yassine-4 <125519755+Yassine-4@users.noreply.github.com>
2023-10-10 00:14:34 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand(98) - RAND_MAX / 2;
 	/* your code goes there */
-	if (n > 98)
+	if (n > 0)
 		printf("is positive\n");
 	else if (n == 0)
 		printf("is zero\n");
-	else if (n < -98)
+	else if (n < 0)
 		printf("is negative\n");
 	return (0);
 }
