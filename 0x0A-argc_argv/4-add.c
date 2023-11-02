67034b0febe0355c53d7657e16d940602c57b55b
Yassine-4 <Yassine-4@gmail.com>
2023-11-01 18:58:25 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,16 +23,6 @@ int check_num(char *str)
 		return (1);
 	}
 
-
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-
 	int main(int argc, char *argv[])
 
 
